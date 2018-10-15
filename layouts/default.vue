3bae24bba60ab79fd8c3346da322f97d9c328c98
sKawashima <bysk.333@gmail.com>
2018-10-15 20:51:45 +0900

    make layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template lang='pug'>
-v-app(dark='')
+v-app(dark)
   v-navigation-drawer(:mini-variant='miniVariant', :clipped='clipped', v-model='drawer', fixed='', app='')
     v-list
       v-list-tile(router='', :to='item.to', :key='i', v-for='(item, i) in items', exact='')
@@ -9,27 +9,29 @@ v-app(dark='')
           v-list-tile-title(v-text='item.title')
   v-toolbar(fixed='', app='', :clipped-left='clipped')
     v-toolbar-side-icon(@click='drawer = !drawer')
-    v-btn(icon='', @click.stop='miniVariant = !miniVariant')
-      v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
-    v-btn(icon='', @click.stop='clipped = !clipped')
-      v-icon web
-    v-btn(icon='', @click.stop='fixed = !fixed')
-      v-icon remove
+    //- v-btn(icon='', @click.stop='miniVariant = !miniVariant')
+    //-   v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
+    //- v-btn(icon='', @click.stop='clipped = !clipped')
+    //-   v-icon web
+    //- v-btn(icon='', @click.stop='fixed = !fixed')
+    //-   v-icon remove
     v-toolbar-title(v-text='title')
     v-spacer
-    v-btn(icon='', @click.stop='rightDrawer = !rightDrawer')
-      v-icon menu
+    //- v-btn(icon='', @click.stop='rightDrawer = !rightDrawer')
+    //-   v-icon menu
   v-content
     v-container
       nuxt
-  v-navigation-drawer(temporary='', :right='right', v-model='rightDrawer', fixed='')
-    v-list
-      v-list-tile(@click.native='right = !right')
-        v-list-tile-action
-          v-icon(light='') compare_arrows
-        v-list-tile-title Switch drawer (click me)
+  //- v-navigation-drawer(temporary='', :right='right', v-model='rightDrawer', fixed='')
+  //-   v-list
+  //-     v-list-tile(@click.native='right = !right')
+  //-       v-list-tile-action
+  //-         v-icon(light='') compare_arrows
+  //-       v-list-tile-title Switch drawer (click me)
   v-footer(:fixed='fixed', app='')
-    span © 2017
+    v-spacer
+    span © 2018 sKawashima
+    v-spacer
 </template>
 
 <script>
@@ -37,16 +39,16 @@ export default {
   data () {
     return {
       clipped: false,
-      drawer: true,
+      drawer: false,
       fixed: false,
       items: [
-        { icon: 'apps', title: 'Welcome', to: '/' },
-        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        { icon: 'keyboard', title: 'Keyboard Input', to: '/' }
+        // { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Key-Number Inputer'
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template lang='pug'>
 v-layout(column='', justify-center='', align-center='')
   v-flex(xs12='', sm8='', md6='')
-    .text-xs-center
-      logo
-      vuetify-logo
     v-card
       v-card-title.headline Welcome to the Vuetify + Nuxt.js template
       v-card-text
@@ -36,13 +33,13 @@ v-layout(column='', justify-center='', align-center='')
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  // components: {
+  //   Logo,
+  //   VuetifyLogo
+  // }
 }
 </script>
